ede5abd9b17b384f2fbf0f66e10ccef3f43f0fec
libin <libin@douyuxingchen.com>
2023-12-15 13:43:09 +0800

    example

--- src/Example/Stateless/Sms.php
+++ src/Example/Stateless/Sms.php
@@ -4,5 +4,23 @@ namespace Douyuxingchen\PhpPkgDemo\Example\Stateless;
 
 class Sms
 {
+    private $appKey;
+    private $appSecret;
+
+    public function __construct(string $appKey, $appSecret)
+    {
+        $this->appKey = $appKey;
+        $this->appSecret = $appSecret;
+    }
+
+    public function setTemplate(string $template)
+    {
+
+    }
+
+    public function send()
+    {
+
+    }
 
 }
